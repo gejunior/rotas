ab00c1923dd0b7db334ff5d62a9f2e47b59dc295
genilson <genilson_junior14@outlook.com>
2025-12-18 22:51:30 -0300

    adicionando templates

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+
 
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
